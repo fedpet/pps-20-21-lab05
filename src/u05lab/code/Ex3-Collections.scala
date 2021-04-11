f06c5d8b8f79b306f61ed410518a27606419eec8
fedpet <federico.pettinari2@studio.unibo.it>
2021-04-11 15:07:22 +0200

    Ex 3

--- src/u05lab/code/Ex3-Collections.scala
+++ src/u05lab/code/Ex3-Collections.scala
@@ -1,8 +1,9 @@
 package u05lab.code
 
 import java.util.concurrent.TimeUnit
-
+import scala.collection.{immutable, mutable}
 import scala.concurrent.duration.FiniteDuration
+import scala.util.Random
 
 object PerformanceUtils {
   case class MeasurementResults[T](result: T, duration: FiniteDuration) extends Ordered[MeasurementResults[_]] {
@@ -13,27 +14,135 @@ object PerformanceUtils {
     val startTime = System.nanoTime()
     val res = expr
     val duration = FiniteDuration(System.nanoTime()-startTime, TimeUnit.NANOSECONDS)
-    if(!msg.isEmpty) println(msg + " -- " + duration.toNanos + " nanos; " + duration.toMillis + "ms")
+    if(msg.nonEmpty) println(msg + " -- " + duration.toNanos + " nanos; " + duration.toMillis + "ms")
     MeasurementResults(res, duration)
   }
 
   def measure[T](expr: => T): MeasurementResults[T] = measure("")(expr)
 }
 
-
 object CollectionsTest extends App {
+  import PerformanceUtils._
+  val numElements = 1000000
+  val elements = 1 to numElements
+  val expensiveUpdateQueriesRange = (numElements/2 - 50) to (numElements/2 + 50)
+  val expensiveReadQueriesRange =
+    ((numElements/2 - 50) to (numElements/2 + 50)) concat // elements "in the middle"
+    (-25 to 0) concat // non-existent elements
+    (numElements to numElements + 25) // non-existent elements
+
+  implicit class ExtendedSeq[A](val list: scala.List[A]) {
+    // "extension-method" with reified type B
+    // example: List("a", 1).only[String](println) will print "a"
+    def only[B: Manifest](f: B => Unit): Unit = {
+      list.filter(e => manifest[B].runtimeClass.isAssignableFrom(e.getClass))
+        .map(_.asInstanceOf[B])
+        .foreach(f)
+    }
+  }
+  def create[A, B](input: Seq[A])(creator: Seq[A] => B): B = {
+    measure(creator(Seq()).getClass.getCanonicalName + " create") {
+      creator(input)
+    }.result
+  }
+  def iterableQueries[A, B](coll: scala.collection.Iterable[A]): Unit = {
+    measure(coll.getClass.getCanonicalName + " size") {
+      coll.size
+    }
+    measure(coll.getClass.getCanonicalName + " foreach") {
+      coll.foreach(_ => {})
+    }
+    measure(coll.getClass.getCanonicalName + " last") {
+      coll.last
+    }
+  }
 
   /* Linear sequences: List, ListBuffer */
-
-  /* Indexed sequences: Vector, Array, ArrayBuffer */
+  val lists = scala.List[Seq[Int] => scala.collection.Seq[Int]](
+    immutable.List.apply[Int],
+    immutable.Queue.apply[Int],
+    immutable.Vector.apply[Int],
+    immutable.ArraySeq.apply[Int],
+    mutable.ListBuffer.apply[Int],
+    mutable.ArrayBuffer.apply[Int]
+  ).map(create(elements))
+  lists.foreach(iterableQueries)
+  lists.foreach(coll => {
+    measure(coll.getClass.getCanonicalName + " appendedAll") {
+      coll.appendedAll(elements)
+    }
+    measure(coll.getClass.getCanonicalName + " contains") {
+      expensiveReadQueriesRange.foreach(coll.contains)
+    }
+    measure(coll.getClass.getCanonicalName + " updated") {
+      expensiveUpdateQueriesRange.foreach(coll.updated(_, 0))
+    }
+    measure(coll.getClass.getCanonicalName + " reverse") {
+      coll.reverse
+    }
+  })
+  lists.only[mutable.Seq[Int]](coll => {
+      measure(coll.getClass.getCanonicalName + " update") {
+        expensiveUpdateQueriesRange.foreach(coll.update(_, 0))
+      }
+  })
 
   /* Sets */
+  val sets = scala.List[Seq[Int] => scala.collection.Set[Int]](
+    immutable.BitSet.apply,
+    immutable.HashSet.apply[Int],
+    immutable.TreeSet.apply[Int],
+    mutable.BitSet.apply,
+    mutable.HashSet.apply[Int],
+    mutable.TreeSet.apply[Int]
+  ).map(create(elements))
+  sets.foreach(iterableQueries)
+  sets.foreach(coll => {
+    measure(coll.getClass.getCanonicalName + " contains") {
+      expensiveReadQueriesRange.foreach(coll.contains)
+    }
+    val testSet = sets.head.map(_ + numElements/2)
+    measure(coll.getClass.getCanonicalName + " union") {
+      coll.union(testSet)
+    }
+    measure(coll.getClass.getCanonicalName + " diff") {
+      coll.diff(testSet)
+    }
+    measure(coll.getClass.getCanonicalName + " intersect") {
+      coll.intersect(testSet)
+    }
+  })
+  sets.only[mutable.Set[Int]](coll => {
+    val anotherSet = sets.head.map(_ + numElements/2)
+    measure(coll.getClass.getCanonicalName + " add") {
+      coll.addAll(anotherSet)
+    }
+    measure(coll.getClass.getCanonicalName + " remove") {
+      elements.foreach(coll.remove)
+    }
+  })
 
   /* Maps */
-
-  /* Comparison */
-  import PerformanceUtils._
-  val lst = (1 to 1000000).toList
-  val vec = (1 to 1000000).toVector
-  assert( measure("lst last"){ lst.last } > measure("vec last"){ vec.last } )
+  val maps = scala.List[Seq[(String, Int)] => scala.collection.Map[String, Int]](
+    immutable.HashMap.apply[String, Int],
+    immutable.TreeMap.apply[String, Int],
+    mutable.HashMap.apply[String, Int],
+    mutable.TreeMap.apply[String, Int]
+  ).map(create(elements.map(_.toString).zipWithIndex))
+  maps.foreach(iterableQueries)
+  maps.foreach(coll => {
+    measure(coll.getClass.getCanonicalName + " contains") {
+      expensiveReadQueriesRange.map(_.toString).foreach(coll.contains)
+    }
+  })
+  maps.only[mutable.Map[String, Int]](coll => {
+    val testKeys = elements.map(_.toString)
+    val testEls = testKeys.zipWithIndex
+    measure(coll.getClass.getCanonicalName + " add") {
+      coll.addAll(testEls)
+    }
+    measure(coll.getClass.getCanonicalName + " remove") {
+      testKeys.foreach(coll.remove)
+    }
+  })
 }
